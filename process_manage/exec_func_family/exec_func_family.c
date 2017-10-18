cc7a8389d2b4f1041f030766980fe715f46eb50a
linux-downey <1148256554@qq.com>
2017-10-18 09:00:50 -0700

    exec族函数 还是没搞懂

--- process_manage/exec_func_family/exec_func_family.c
+++ process_manage/exec_func_family/exec_func_family.c
@@ -7,9 +7,28 @@
  */
 #include <unistd.h>
 #include <stdio.h>
-
+#include <errno.h>
 int main(int argc,char *argv[])
 {
-	execlp("/bin/ls",NULL);
+		pid_t pid;
+		int status=0;
+		printf("fork demo\r\n");
+		pid =fork();
+		if(0==pid)
+		{
+			printf("this is child process,pid=%d\r\n",getpid());
+		//	execlp("/home/downey/test_file","test_file",NULL);
+			execl("/home/downey/test_file","test_file",NULL);
+			printf("111");
+			printf("%s\r\n",strerror(errno));
+		}
+		else
+		{
+			printf("this is father process,child pid =%d\r\n",pid);
+			//wait(&status);
+			waitpid(pid,&status,0);
+			printf("child process %d exit with %d\r\n",pid,status);
+		}
+
 	return 1;
 }
